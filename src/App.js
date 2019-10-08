4d776541d1b1298e9669e62a67736ddc5a1722db
marlonchiu <Knight1898@outlook.com>
2019-10-08 17:20:35 +0800

    redux 相关引入及基础搭建

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react'
+import { Provider } from 'react-redux'
 import { IconStyle } from './assets/iconfont/iconfont'
 import { GlobalStyle } from './style'
 import { renderRoutes } from 'react-router-config'
 import routes from './routes/index'
+import store from './store/index'
 import { HashRouter } from 'react-router-dom'
 
 function App () {
   return (
-    <div className='App'>
+    <Provider store={store}>
       <HashRouter>
         <GlobalStyle />
         <IconStyle />
         {renderRoutes(routes)}
       </HashRouter>
-    </div>
+    </Provider>
   )
 }
 
