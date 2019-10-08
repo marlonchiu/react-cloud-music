b12df240b6e83e567fb91921ded8b16541229128
marlonchiu <Knight1898@outlook.com>
2019-10-08 18:00:23 +0800

    recommend模块 轮播banner开发

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Slider from '../../components/slider'
+
 function Recommend (props) {
+  // mock数据
+  const bannerList = [1, 2, 3, 4].map(item => {
+    return { imageUrl: 'http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg' }
+  })
+
   return (
-    <div>Recommend</div>
+    <div>
+      <Slider bannerList={bannerList} />
+    </div>
   )
 }
 export default React.memo(Recommend)
