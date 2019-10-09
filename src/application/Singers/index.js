df6fdc8602c203cba555e9b335a2af0a5e2dcd6b
marlonchiu <Knight1898@outlook.com>
2019-10-09 16:41:35 +0800

    歌手列表模块开发 歌手列表部分的开发mock模拟数据

--- src/application/Singers/index.js
+++ src/application/Singers/index.js
@@ -1,7 +1,36 @@
 import React, { useState } from 'react'
 import Horizen from '../../baseUI/horizen-item'
 import { categoryTypes, alphaTypes } from '../../api/config'
-import { NavContainer } from './style'
+import { NavContainer, ListContainer, List, ListItem } from './style'
+import Scroll from '../../baseUI/scroll/index'
+
+// mock数据
+const singerList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(item => {
+  return {
+    picUrl: 'https://p2.music.126.net/uTwOm8AEFFX_BYHvfvFcmQ==/109951164232057952.jpg',
+    name: '隔壁老樊',
+    accountId: 277313426,
+  }
+})
+// 渲染函数，返回歌手列表
+const renderSingerList = () => {
+  return (
+    <List>
+      {
+        singerList.map((item, index) => {
+          return (
+            <ListItem key={item.accountId + '-' + index}>
+              <div className='img_wrapper'>
+                <img src={item.picUrl + '?param=300x300'} width='100%' height='100%' alt='music' />
+              </div>
+              <span className='name'>{item.name}</span>
+            </ListItem>
+          )
+        })
+      }
+    </List>
+  )
+}
 
 function Singers (props) {
   const [category, setCategory] = useState('')
@@ -15,20 +44,27 @@ function Singers (props) {
   }
 
   return (
-    <NavContainer>
-      <Horizen
-        list={categoryTypes}
-        title={'分类（默认热门）:'}
-        oldVal={category}
-        handleClick={(val) => handleUpdateCatetory(val)}
-      />
-      <Horizen
-        list={alphaTypes}
-        title='首字母:'
-        oldVal={alpha}
-        handleClick={(val) => handleUpdateAlpha(val)}
-      />
-    </NavContainer>
+    <div>
+      <NavContainer>
+        <Horizen
+          list={categoryTypes}
+          title={'分类（默认热门）:'}
+          oldVal={category}
+          handleClick={(val) => handleUpdateCatetory(val)}
+        />
+        <Horizen
+          list={alphaTypes}
+          title='首字母:'
+          oldVal={alpha}
+          handleClick={(val) => handleUpdateAlpha(val)}
+        />
+      </NavContainer>
+      <ListContainer>
+        <Scroll>
+          {renderSingerList()}
+        </Scroll>
+      </ListContainer>
+    </div>
   )
 }
 
