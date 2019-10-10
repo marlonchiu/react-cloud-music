28830c12f0f8bfe0d02f0610490499373c134ee5
marlonchiu <Knight1898@outlook.com>
2019-10-10 20:42:17 +0800

    歌单详情模块 header 组件开发

--- src/application/Album/index.js
+++ src/application/Album/index.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react'
 import { Container } from './style'
 import { CSSTransition } from 'react-transition-group'
+import Header from '../../baseUI/header/index'
 
 function Album (props) {
   const [showStatus, setShowStatus] = useState(true)
 
+  const handleBack = () => {
+    setShowStatus(false)
+  }
+
+  // 在退出动画执行结束时跳转路由
+  // onExited={props.history.goBack}
   return (
     <CSSTransition
-      in={showStatus}  
+      in={showStatus}
       timeout={300}
-      appear={true}
       classNames='fly'
+      appear={true}
       unmountOnExit
       onExited={props.history.goBack}
     >
-      <Container>Album</Container>
+      <Container>
+        <Header title='返回' handleClick={handleBack} />
+      </Container>
     </CSSTransition>
   )
 }
