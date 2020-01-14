d05117ad6d27d9d1dca2a957f23c6f6af6911b81
marlonchiu <Knight1898@outlook.com>
2020-01-14 20:53:14 +0800

    修复播放器播放时间 方法修复

--- src/api/utils.js
+++ src/api/utils.js
@@ -82,8 +82,8 @@ export const getSongUrl = id => {
 
 // 转换歌曲播放时间
 export const formatPlayTime = interval => {
-  interval = interval || 0
-  const minute = (interval / 60) || 0
+  interval = interval | 0
+  const minute = (interval / 60) | 0
   const second = (interval % 60).toString().padStart(2, '0')
   return `${minute}:${second}`
 }
